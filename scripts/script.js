a95942221f390efe85051a3336ce8602616db8b8
Marvin-way <danis210594@gmail.com>
2021-10-19 22:59:53 +0300

    fix: исправлен размер фона кнопки *+* в разрешении 320, удалены дебагер из скрипта, приведен в более читабельный код, исправлены блоки page и popup__save-button

--- scripts/script.js
+++ scripts/script.js
@@ -14,17 +14,14 @@ let profileSubTitle = page.querySelector('.profile__subtitle');
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  console.log(`open popup`);
-  popupName.setAttribute('value',`${profileTitle.textContent}`);
-  popupJob.setAttribute('value',`${profileSubTitle.textContent}`);
+  popupName.value = profileTitle.textContent;
+  popupJob.value = profileSubTitle.textContent;
 }
 function closePopup(evt) {
   popup.classList.remove('popup_opened');
-  console.log(`close popup`);
 }
 function handleProfileFormSubmit (evt) {
     evt.preventDefault();
-    console.log(`completed`);
     profileTitle.textContent = popupName.value;
     profileSubTitle.textContent = popupJob.value;
     closePopup();
