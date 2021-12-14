481b32e6e6a24cc1dd1cab306ee7390f7295c0c7
Marvin-way <danis210594@gmail.com>
2021-12-14 20:02:43 +0300

    fix: Добавить в конструктор класса новые параметры, для оптимизации класса

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,98 +1,12 @@
-/////////////////////////////////////////////////////////////////////////
-////////////    Находим все нужные для алгоритма узлы     ///////////////
-/////////////////////////////////////////////////////////////////////////
-const page = document.querySelector('.page');
-const editButton = page.querySelector('.profile__edit-button');
-const closeButtonProfile = page.querySelector('.popup__close-button_profile');
-const closeButtonCard = page.querySelector('.popup__close-button_card');
-const closeButtonView = page.querySelector('.popup__close-button_view');
-const addButton = page.querySelector('.profile__add-button');
-const elementsList = page.querySelector('.element');
-const imageView = page.querySelector('.popup-view');
-const imageViewImg = page.querySelector('.popup-view__image');
-const imageViewCaption = page.querySelector('.popup-view__caption');
-
-const popups = page.querySelectorAll('.popup');
-const popupProfile = page.querySelector('.popup_profile');
-const popupCard = page.querySelector('.popup_card');
-const popupFormProfile = page.querySelector('.popup__form_profile');
-const popupFormCard = page.querySelector('.popup__form_card');
-const popupName = page.querySelector('.popup__input_name');
-const popupJob = page.querySelector('.popup__input_job');
-const popupPlace = page.querySelector('.popup__input_place');
-const popupLink = page.querySelector('.popup__input_link');
-const popupButtonCard = popupFormCard.querySelector('.popup__button');
-
-const profileTitle = page.querySelector('.profile__title');
-const profileSubTitle = page.querySelector('.profile__subtitle');
-
-const ESC_CODE = 'Escape';
-/////////////////////////////////////////////////////////////////////////
-///////////////////      Основные скрипты тут     ///////////////////////
-/////////////////////////////////////////////////////////////////////////
-function renderCard(name, link) {
-  elementsList.prepend(addCard(name, link));
-}
-function addCard(name, link) {
-  const card = new Card(name, link, '#create-element');
-  const elementCard = card.generateCard();
-  return elementCard;
-}
-function openPopup(thisPopup) {
-  thisPopup.classList.add('popup_opened');
-  document.addEventListener('keydown', closePopupEscape);
-}
-function closePopup(thisPopup) {
-  thisPopup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closePopupEscape);
-}
-function handleProfileFormOpen(popupProfile) {
-  popupName.value = profileTitle.textContent;
-  popupJob.value = profileSubTitle.textContent;
-  openPopup(popupProfile);
-}
-function handleProfileFormSubmit(evt) {
-  profileTitle.textContent = popupName.value;
-  profileSubTitle.textContent = popupJob.value;
-  closePopup(popupProfile);
-}
-function handleCardFormSubmit(evt) {
-  renderCard(popupPlace.value, popupLink.value);
-  closePopup(popupCard);
-  popupPlace.value = "";
-  popupLink.value = "";
-  popupButtonCard.disabled = true;
-  popupButtonCard.classList.add('popup__button_disabled');
-}
-
-function closePopupEscape(evt) {
-  if(evt.key === ESC_CODE) {
-    const openedPopup = page.querySelector('.popup_opened');
-    closePopup(openedPopup);
-  }
-}
-/////////////////////////////////////////////////////////////////////////
-////////////      Устанавливаем слежку за событиями    //////////////////
-/////////////////////////////////////////////////////////////////////////
-editButton.addEventListener('click', () => handleProfileFormOpen(popupProfile));
-addButton.addEventListener('click', () => openPopup(popupCard));
-closeButtonProfile.addEventListener('click', () => closePopup(popupProfile));
-closeButtonCard.addEventListener('click', () => closePopup(popupCard));
-closeButtonView.addEventListener('click', () => closePopup(imageView));
-popupFormProfile.addEventListener('submit', handleProfileFormSubmit);
-popupFormCard.addEventListener('submit', handleCardFormSubmit);
-popups.forEach((popup) => {
-  popup.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('popup_opened')){
-      closePopup(popup);
-    };
-  })
-})
 class Card {
-  constructor(name, link, cardTemplate){
+  constructor(name, link, cardTemplate, imageView,imageViewImg, imageViewCaption, openPopup){
     this._name = name,
     this._link = link,
-    this._cardTemplate = cardTemplate
+    this._cardTemplate = cardTemplate, 
+    this._cardView = imageView,
+    this._cardImage = imageViewImg, 
+    this._cardCaption = imageViewCaption,
+    this._cardPopup = openPopup
   }
   _getTemplate() {
     const cardElement = document
@@ -112,14 +26,14 @@ class Card {
     this._setEventsListeners();
     this._item.querySelector('.element__image').src = this._link;
     this._item.querySelector('.element__name').textContent = this._name;
-    this._item.querySelector('.element__name').alt = this._name;
+    this._item.querySelector('.element__image').alt = this._name;
     return this._item;
   }
   _showImage(){
-    imageViewImg.src = this._link;
-    imageViewImg.alt = `Картинка ${this._name}`;
-    imageViewCaption.textContent = this._name;
-    openPopup(imageView);
+    this._cardImage.src = this._link;
+    this._cardImage.alt = `Картинка ${this._name}`;
+    this._cardCaption.textContent = this._name;
+    this._cardPopup(this._cardView);
   }
   _likeButton(){
     this._item.querySelector('.element__button').classList.toggle('element__button_active');
--- scripts/Script.js
+++ scripts/Script.js
@@ -29,11 +29,98 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+/////////////////////////////////////////////////////////////////////////
+////////////    Находим все нужные для алгоритма узлы     ///////////////
+/////////////////////////////////////////////////////////////////////////
+const page = document.querySelector('.page');
+const editButton = page.querySelector('.profile__edit-button');
+const closeButtonProfile = page.querySelector('.popup__close-button_profile');
+const closeButtonCard = page.querySelector('.popup__close-button_card');
+const closeButtonView = page.querySelector('.popup__close-button_view');
+const addButton = page.querySelector('.profile__add-button');
+const elementsList = page.querySelector('.element');
+const imageView = page.querySelector('.popup-view');
+const imageViewImg = page.querySelector('.popup-view__image');
+const imageViewCaption = page.querySelector('.popup-view__caption');
+
+const popups = page.querySelectorAll('.popup');
+const popupProfile = page.querySelector('.popup_profile');
+const popupCard = page.querySelector('.popup_card');
+const popupFormProfile = page.querySelector('.popup__form_profile');
+const popupFormCard = page.querySelector('.popup__form_card');
+const popupName = page.querySelector('.popup__input_name');
+const popupJob = page.querySelector('.popup__input_job');
+const popupPlace = page.querySelector('.popup__input_place');
+const popupLink = page.querySelector('.popup__input_link');
+const popupButtonCard = popupFormCard.querySelector('.popup__button');
+
+const profileTitle = page.querySelector('.profile__title');
+const profileSubTitle = page.querySelector('.profile__subtitle');
+
+const ESC_CODE = 'Escape';
+/////////////////////////////////////////////////////////////////////////
+///////////////////      Основные скрипты тут     ///////////////////////
+/////////////////////////////////////////////////////////////////////////
+function renderCard(name, link) {
+  elementsList.prepend(addCard(name, link));
+}
+function addCard(name, link) {
+  const card = new Card(name, link, '#create-element', imageView, imageViewImg, imageViewCaption, openPopup);
+  const elementCard = card.generateCard();
+  return elementCard;
+}
+function openPopup(thisPopup) {
+  thisPopup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupEscape);
+}
+function closePopup(thisPopup) {
+  thisPopup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEscape);
+}
+function handleProfileFormOpen(popupProfile) {
+  popupName.value = profileTitle.textContent;
+  popupJob.value = profileSubTitle.textContent;
+  openPopup(popupProfile);
+}
+function handleProfileFormSubmit(evt) {
+  profileTitle.textContent = popupName.value;
+  profileSubTitle.textContent = popupJob.value;
+  closePopup(popupProfile);
+}
+function handleCardFormSubmit(evt) {
+  renderCard(popupPlace.value, popupLink.value);
+  closePopup(popupCard);
+  popupPlace.value = "";
+  popupLink.value = "";
+}
+
+function closePopupEscape(evt) {
+  if(evt.key === ESC_CODE) {
+    const openedPopup = page.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  }
+}
+/////////////////////////////////////////////////////////////////////////
+////////////      Устанавливаем слежку за событиями    //////////////////
+/////////////////////////////////////////////////////////////////////////
+editButton.addEventListener('click', () => handleProfileFormOpen(popupProfile));
+addButton.addEventListener('click', () => openPopup(popupCard));
+closeButtonProfile.addEventListener('click', () => closePopup(popupProfile));
+closeButtonCard.addEventListener('click', () => closePopup(popupCard));
+closeButtonView.addEventListener('click', () => closePopup(imageView));
+popupFormProfile.addEventListener('submit', handleProfileFormSubmit);
+popupFormCard.addEventListener('submit', handleCardFormSubmit);
+popups.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup_opened')){
+      closePopup(popup);
+    };
+  })
+})
 function startPage () {
     initialCards.forEach(item => {
-    const card = new Card(item.name, item.link, '#create-element');
-    const y = card.generateCard();
-    return document.querySelector('.element').prepend(y);
+    const card = renderCard(item.name, item.link);
+    return card;
   })
 };
 startPage();
